b0afd490c7ed92f1a166770b836147da928b1f19
Justin Higgins <justin@selfless.io>
2021-11-21 22:32:41 -0800

    update the right file

--- .github/actions/extractHTMLTitle/index.js
+++ .github/actions/extractHTMLTitle/index.js
@@ -3,13 +3,13 @@ const { parse } = require("node-html-parser");
 
 const {
   INPUT_REPO_TOKEN,
-  ADDED_FILE_NAME
+  INPUT_ADDED_FILE_PATH
 } = process.env;
 
 console.log('--------------------------');
 console.log({
   INPUT_REPO_TOKEN,
-  ADDED_FILE_NAME
+  INPUT_ADDED_FILE_PATH
 });
 
 async function run() {
@@ -21,7 +21,7 @@ async function run() {
   const content = octokit.rest.repos.getContent({
     owner: github.context.payload.repository.owner.name,
     repo: github.context.payload.repository.name,
-    path: ADDED_FILE_NAME
+    path: INPUT_ADDED_FILE_PATH
   });
   console.log('CONTENT----------------');
   console.log(content);
